03d324d9a682f6029b1585096c30f628a0bbf5c0
Cerburus <Cerburus@DESKTOP-VLKB8ND>
2026-02-03 21:54:19 -0700

    First Update

--- activities/activity3/topic3_1/src/main/java/com/gcu/business/OrdersBusinessService.java
+++ activities/activity3/topic3_1/src/main/java/com/gcu/business/OrdersBusinessService.java
@@ -2,6 +2,7 @@ package com.gcu.business;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import com.gcu.model.OrderModel;
 
 public class OrdersBusinessService implements OrdersBusinessServiceInterface {
@@ -15,9 +16,9 @@ public class OrdersBusinessService implements OrdersBusinessServiceInterface {
     public List<OrderModel> getOrders() {
         List<OrderModel> orders = new ArrayList<>();
 
-        orders.add(new OrderModel(Long.parseLong("0000001"), "0000001", "Product 1", 9.99f, 1));
-        orders.add(new OrderModel(Long.parseLong("0000002"), "0000002", "Product 2", 19.99f, 2));
-        orders.add(new OrderModel(Long.parseLong("0000003"), "0000003", "Product 3", 29.99f, 3));
+        orders.add(new OrderModel(Long.valueOf("0000001"), "0000001", "Product 1", 9.99f, 1));
+        orders.add(new OrderModel(Long.valueOf("0000002"), "0000002", "Product 2", 19.99f, 2));
+        orders.add(new OrderModel(Long.valueOf("0000003"), "0000003", "Product 3", 29.99f, 3));
 
         return orders;
     }
