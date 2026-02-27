f7433d3cce73bac1fdfb05db7a7e707d905183b2
Cerburus <Cerburus@DESKTOP-VLKB8ND>
2026-02-26 20:38:39 -0700

    PArt 4 Update

---
+++ activities/activity6/topic6_4/src/main/java/com/gcu/controllers/MyService.java
@@ -0,0 +1,18 @@
+package com.gcu.controllers;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service")
+public class MyService {
+
+    @GetMapping("/test")
+    public String test(Principal principal) {
+        return "hello " + principal.getName() + ", you have successfully authenticated with GitHub!";
+    }
+
+}
